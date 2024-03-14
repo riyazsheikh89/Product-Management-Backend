96c416a1686ab5c0fce24c8d1dfb7b6ec32d4605
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2024-03-14 19:10:35 +0530

    added product-controller with route

---
+++ src/controller/product-controller.js
@@ -0,0 +1,55 @@
+const Product = require("../models/Product");
+
+// API: /api/v1/get-products?page=2
+const getProducts = async (req, res) => {
+    try {
+        const page = parseInt(req.query.page) || 1;
+        const pageSize = 10;
+        const skipCount = (page - 1) * pageSize;
+
+        const products = await Product.find()
+            .skip(skipCount)
+            .limit(pageSize);
+
+        return res.status(200).json({
+            success: true,
+            message: "Successfuly fetched all products",
+            data: products,
+            err: {}
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Unable to fetch all products!",
+            data: {},
+            err: error
+        });
+    }
+}
+
+// API: /api/v1//product/:id
+const getProduct = async (req, res) => {
+    try {
+        const productId = req.params.id;
+        const product = await Product.findById(productId);
+        
+        return res.status(200).json({
+            success: true,
+            message: "Successfuly fetched the product",
+            data: product,
+            err: {}
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Unable to fetch the product!",
+            data: {},
+            err: error
+        });
+    }
+}
+
+module.exports = {
+    getProducts,
+    getProduct
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 
 const { signup, login } = require("../controller/user-controller");
+const { getProducts, getProduct } = require("../controller/product-controller");
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 
+router.get("/get-products", getProducts);   // multiple
+router.get("/product/:id", getProduct);     // single
 
 module.exports = router;

0ca84c572e695b1686ffb7f57726969ce0c4bc22
Sheikh Riyaz Ahmed <wbriyaz89@gmail.com>
2024-03-15 14:47:27 +0530

    added update review route

--- src/controller/review-controller.js
+++ src/controller/review-controller.js
@@ -19,7 +19,37 @@ const createReview = async (req, res) => {
     }
 }
 
+const updateReviewStatus = async (req, res) => {
+    try {
+        const reviewId = req.params.id;
+        const data = {
+            status: req.body.status,
+            admin: req.user._id
+        }
+        console.log(reviewId, data);
+        const response = await Review.findOneAndUpdate(
+            {_id: reviewId},
+            data,
+            {new: true}
+        );
+        return res.status(201).json({
+            success: true,
+            message: "Successfuly updated your review",
+            data: response,
+            err: {}
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Unable to update the review!",
+            data: {},
+            err: error
+        });
+    }
+}
+
 
 module.exports = {
     createReview,
+    updateReviewStatus
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const { signup, login } = require("../controller/user-controller");
 const { getProducts, getProduct, updateProduct } = require("../controller/product-controller");
-const { createReview } = require("../controller/review-controller");
+const { createReview, updateReviewStatus } = require("../controller/review-controller");
 
 const authentication = require('../middlewares/authentication');
+const authorization = require('../middlewares/authorization');
 
 const router = express.Router();
 
@@ -16,5 +17,6 @@ router.get("/product/:id", authentication, getProduct);     // single
 router.patch("/product/update/:id", authentication, updateProduct);   // update a specific product
 
 router.post("/review/create", authentication, createReview);    // creates a review product
+router.patch("/review/update/:id", authentication, authorization, updateReviewStatus);
 
 module.exports = router;
